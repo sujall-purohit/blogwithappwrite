453a7b32eeb64f8fa3aa07462f95abf187753acc
sujall-purohit <purohitsujal9828@gmail.com>
2026-06-15 19:06:16 +0530

    Responsive UI

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Logo, Container, LogoutBtn } from '../index'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { Menu, X } from "lucide-react";
 
 function Header() {
+    const [open, setOpen] = useState(false);
     const authStatus = useSelector((state) => state.auth.status)
     const navigate = useNavigate()
 
@@ -37,38 +39,57 @@ function Header() {
 
 
     return (
-        <header className="mx-auto w-[80%] sticky top-4 rounded-4xl z-50 bg-gray-400/70 backdrop-blur-xl border-b border-slate-200 shadow-[0_2px_10px_rgba(0,0,0,0.05)] h-20 flex  items-center text-xl
+        <header className="mx-auto w-[95%] md:w-[80%] sticky top-4 rounded-4xl z-50 bg-gray-400/70 backdrop-blur-xl border-b border-slate-200 shadow-[0_2px_10px_rgba(0,0,0,0.05)] h-20 flex  items-center text-xl
 ">
             <Container className="max-w-7xl mx-auto px-6">
                 <nav className="flex items-center justify-between h-full">
+                    {open && (
+                        <ul className="md:hidden absolute top-24 left-0 w-full bg-gray-400/90 backdrop-blur-xl rounded-2xl p-4 flex flex-col gap-2">
+                            {navItems.map((item) =>
+                                item.active ? (
+                                    <li key={item.name}>
+                                        <button
+                                            onClick={() => {
+                                                navigate(item.slug)
+                                                setOpen(false)
+                                            }}
+                                            className="w-full text-left px-4 py-2 rounded-xl hover:bg-indigo-50"
+                                        >
+                                            {item.name}
+                                        </button>
+                                    </li>
+                                ) : null
+                            )}
+
+                            {authStatus && (
+                                <li>
+                                    <LogoutBtn />
+                                </li>
+                            )}
+                        </ul>
+                    )}
                     <div>
                         <Link to="/">
-                            <Logo width="90px" />
+                            <Logo width="70px" className="md:w-[90px]" />
                         </Link>
                     </div>
-                    <ul className="flex items-center gap-2 ml-auto">
+                    <button
+                        onClick={() => setOpen(!open)}
+                        className="md:hidden ml-auto mr-5">
+                        {open ? <X size={28} /> : <Menu size={28} />}
+                    </button>
+                    <ul className=" hidden md:flex items-center gap-2 ml-auto">
                         {navItems.map((item) =>
                             item.active ? (
                                 <li key={item.name}>
                                     <button
                                         onClick={() => navigate(item.slug)}
-                                        className="
-px-4
-py-2
-text-slate-600
-font-medium
-rounded-xl
-transition-all
-duration-300
-hover:bg-indigo-50
-hover:text-indigo-600
-">
+                                        className=" px-4 py-2 text-slate-600 font-medium rounded-xl transition-all duration-300 hover:bg-indigo-50 hover:text-indigo-600 ">
                                         {item.name}
                                     </button>
                                 </li>
                             ) : null
                         )}
-
                         {authStatus && (
                             <li>
                                 <LogoutBtn />
